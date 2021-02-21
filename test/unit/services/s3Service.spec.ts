6efaf3f521171e2fbbdbec5c20a73d401935e5b8
josephdempsey <joeadempsey@gmail.com>
2021-02-21 12:47:33 +0000

    test(s3): add unit test for s3 service and scripts to run build and serverless setup

---
+++ test/unit/services/s3Service.spec.ts
@@ -0,0 +1,33 @@
+import { S3Service } from '../../../app/services/s3Service';
+import { config } from '../../../app/config';
+
+const mS3Instance = {
+  upload: jest.fn().mockReturnThis(),
+  getObject: jest.fn().mockReturnThis(),
+  promise: jest.fn(),
+};
+
+jest.mock('aws-sdk', () => ({ S3: jest.fn(() => mS3Instance) }));
+
+describe('s3 Service', () => {
+  it('should throw if upload fails', async () => {
+    const error = new Error('upload failed');
+    mS3Instance.promise.mockRejectedValueOnce(error);
+    const s3Service = new S3Service();
+    try {
+      await s3Service.uploadImage(Buffer.from('Image'), 'fileName');
+    } catch (err) {
+      expect(err).toEqual(error);
+    }
+
+    expect(mS3Instance.upload).toBeCalledWith({ Bucket: config.bucketName, Key: 'fileName', Body: Buffer.from('Image') });
+  });
+
+  it('should upload and return upload information', async () => {
+    mS3Instance.promise.mockResolvedValueOnce('S3 Upload Information');
+    const s3Service = new S3Service();
+    const actual = await s3Service.uploadImage(Buffer.from('Image'), 'fileName');
+    expect(actual).toEqual('S3 Upload Information');
+    expect(mS3Instance.upload).toBeCalledWith({ Bucket: config.bucketName, Key: 'fileName', Body: Buffer.from('Image') });
+  });
+});
